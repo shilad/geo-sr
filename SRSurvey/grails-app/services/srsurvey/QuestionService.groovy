59650a1d407aa9e7c33364dfb2b7cd924fa2db03
shilad <shilad@gmail.com>
2014-07-12 14:32:04 -0500

    Added test. It works!

--- SRSurvey/grails-app/services/srsurvey/QuestionService.groovy
+++ SRSurvey/grails-app/services/srsurvey/QuestionService.groovy
@@ -1,6 +1,7 @@
 package srsurvey
 
 import grails.transaction.Transactional
+import org.wikibrain.spatial.maxima.OrderQuestions
 import org.wikibrain.spatial.maxima.SpatialConcept
 import org.wikibrain.spatial.maxima.SpatialConceptPair
 
@@ -9,42 +10,73 @@ import javax.servlet.http.HttpServletRequest
 @Transactional
 class QuestionService {
     def loggingService
-
-    List<String[]> titles = []
+    OrderQuestions orderer;
+    Map<String, Set<Integer>> neighbors;
 
     def init() {
-        for (String line : new File("dat/IDsToTitles.txt").readLines()) {
-            String [] tokens = line.split(":", 2)
-            titles.add(tokens)
+        orderer = new OrderQuestions();
+        loadNeighborFile(new File("citiesToNeighbors4.txt"));
+        log.info("read neighbors for ${neighbors.size()} cities")
+    }
+
+    /**
+     * Read in a plain-text file whose format consists of the (tab separated) fields
+     * city_country,city_state,city_name    city_population  city_neighbor_1  city_neighbor_2  ...
+     * for each city.
+     * <p/>
+     * Also modifies the cityPopulations map.
+     *
+     * @param file The file we are loading from
+     * @return Map from city id (city_country,city_state,city_name) to city neighbors
+     */
+    public void loadNeighborFile(File file) {
+        neighbors = new HashMap<String, Set<Integer>>();
+        try {
+            Scanner scan = new Scanner(file, "UTF-8");
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                String[] array = line.split("\t");
+                Set<Integer> set = new HashSet<Integer>();
+                String id = array[0];
+                for (int i = 2; i < array.length; i++) {
+                    set.add(Integer.parseInt(array[i]));
+                }
+                neighbors.put(id, set);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        log.info("read information for ${titles.size()} spatial concepts")
     }
 
     def setQuestions(Person p, HttpServletRequest req) {
         if (p.survey == null) {
             throw new IllegalArgumentException()
         }
-        List<SpatialConceptPair> pairs = []
-        List<String> messages = []
-        for (int i = 0; i < 50; i++) {
-            SpatialConceptPair pair = new SpatialConceptPair(pickConcept(), pickConcept())
-            pairs.add(pair)
-            messages.add("p\t${pair.firstConcept}\t${pair.secondConcept}")
+        Set<Integer> knownIds = new HashSet<Integer>();
+        for (LivedInLocation lil : p.homes) {
+            String key = lil.country +"," + lil.state + "," + lil.city;
+            if (!neighbors.containsKey(key)) {
+                log.warn("unknown neighbor location: " + key);
+                continue;
+            }
+            knownIds.addAll(neighbors.get(key));
         }
-        int i = 0
-        for (SpatialConceptPair pair : pairs) {
-            Question q = new Question(i, (i / 10) as int, pair.firstConcept.title, pair.secondConcept.title, p.survey)
-            p.survey.addToQuestions(q)
-            i++
-        }
-        p.save(flush: true)
-        loggingService.appendMany(p, req, messages)
-    }
 
-    def pickConcept() {
-        Random random = new Random()
-        String [] tokens = titles[random.nextInt(titles.size())]
-        SpatialConcept concept = new SpatialConcept(tokens[0] as int, tokens[1])
-        return concept
+        List<String> messages = []
+        List<SpatialConceptPair>[] pages = orderer.getQuestions(knownIds as List, Person.count);
+        int i = 0
+        for (int pageNum = 0; pageNum < pages.length; pageNum++) {
+            for (SpatialConceptPair pair : pages[pageNum]) {
+                Question q = new Question(i, pageNum, pair.firstConcept.title, pair.secondConcept.title, p.survey)
+                p.survey.addToQuestions(q)
+                messages.add("p\t${pair.firstConcept}\t${pair.secondConcept}")
+                i++
+            }
+        }
+
+        p.save(flush: true)
+        if (req != null) {
+            loggingService.appendMany(p, req, messages)
+        }
     }
 }
--- SRSurvey/test/unit/srsurvey/QuestionServiceSpec.groovy
+++ SRSurvey/test/unit/srsurvey/QuestionServiceSpec.groovy
@@ -1,20 +1,41 @@
 package srsurvey
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
-import spock.lang.Specification
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(QuestionService)
-class QuestionServiceSpec extends Specification {
+@Mock([Person, Survey, LivedInLocation, Question])
+public class QuestionServiceSpec {
 
-    def setup() {
+    @Before
+    public void setup() {
+        service.init()
     }
 
-    def cleanup() {
+    @After
+    public void cleanup() {
     }
 
-    void "test something"() {
+    @Test
+    public void testSetQuestions() {
+        Person p = new Person()
+        p.survey = new Survey()
+        p.homes = [
+            new LivedInLocation(
+                country : "United States of America",
+                state : "Minnesota",
+                city : "Minneapolis"
+            )
+        ]
+        service.setQuestions(p, null)
+        for (Question q : p.survey.questions) {
+            System.out.println("question is ${q.location1}, ${q.location2}")
+        }
     }
 }
